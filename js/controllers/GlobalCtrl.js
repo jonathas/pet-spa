787307effe6a4d865e9d6dd5b1b5152f965f2300
Jon Ribeiro <contact@jonathas.com>
2017-11-13 00:47:59 +0100

    Add ng-show for hiding the forms

--- js/controllers/GlobalCtrl.js
+++ js/controllers/GlobalCtrl.js
@@ -1,4 +1,16 @@
 angular.module('petApp')
     .controller('GlobalCtrl', function($scope) {
+        $scope.formFamily = false;
+        $scope.formIndividual = false;
+
+        $scope.showAddFamily = function() {
+            $scope.formFamily = true;
+            $scope.formIndividual = false;
+        };
+
+        $scope.showAddIndividual = function() {
+            $scope.formFamily = false;
+            $scope.formIndividual = true;
+        };
 
     });
